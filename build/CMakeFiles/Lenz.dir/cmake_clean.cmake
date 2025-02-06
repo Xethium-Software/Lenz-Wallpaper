c1a4b52f66d224df459b5d53c3287644f43e82ed
ShaderHex <barkott9@gmail.com>
2025-02-06 22:11:47 +0300

    Cmake and premake setup

---
+++ build/CMakeFiles/Lenz.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lenz.dir/src/main.cpp.o"
+  "CMakeFiles/Lenz.dir/src/main.cpp.o.d"
+  "Lenz"
+  "Lenz.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Lenz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
