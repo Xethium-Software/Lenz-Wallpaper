36b0ba8e4272ed38a392cddad86cdcc021fc8b0d
ShaderHex <barkott9@gmail.com>
2025-02-07 22:06:06 +0300

    Fixed premake

--- build/CMakeFiles/Lenz.dir/src/main.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/Lenz.dir/src/main.cpp.o: \
- /home/ausr/Documents/Projects/Lenz/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/ausr/Documents/Projects/Lenz/external/raylib/src/raylib.h \
- /usr/lib/gcc/x86_64-redhat-linux/14/include/stdarg.h
